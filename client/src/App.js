130b56137b442d882796547ea41d3f6735974e59
smithy98 <smithy_98@outlook.com>
2021-05-07 14:39:08 +0100

    Start button in home comp & init game comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Home from "./components/Home";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/game' exact component={Game} />
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 function Home() {
-  return <div>Hello</div>;
+  const history = useHistory();
+
+  const startGame = () => history.push("/game");
+  return (
+    <Container
+      className='d-flex flex-column justify-content-center align-items-center'
+      style={{ width: "100vh", height: "100vh" }}
+    >
+      <h1 style={{ fontSize: 80 }}>DrawIT</h1>
+      <Button className='m-3' style={{ width: "10vh" }} onClick={startGame}>
+        Start
+      </Button>
+    </Container>
+  );
 }
 
 export default Home;
